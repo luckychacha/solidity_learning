e0446085d1993b8a11be3c89a8b016d54af52233
luckychacha <358140284@qq.com>
2023-03-31 18:36:27 +0800

    add capture.

--- truffle-config.js
+++ truffle-config.js
@@ -101,7 +101,7 @@ module.exports = {
       gas: 10000000,
       gasPrice: 1500000000,
       // confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      // timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
     //
@@ -121,7 +121,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.19", // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.18", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {
         // See the solidity docs for advice about optimization and evmVersion
