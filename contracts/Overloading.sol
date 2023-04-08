824e93c3cfbf065292b47d1599a06a0518b6b634
luckychacha <358140284@qq.com>
2023-04-08 23:58:16 +0800

    lesson 16: Overloading.

--- contracts/Overloading.sol
+++ contracts/Overloading.sol
@@ -11,4 +11,12 @@ contract Overloading {
     ) public pure returns (string memory) {
         return (something);
     }
+
+    function f(uint8 _in) public pure returns (uint8 out) {
+        out = _in;
+    }
+
+    function f(uint256 _in) public pure returns (uint256 out) {
+        out = _in;
+    }
 }
