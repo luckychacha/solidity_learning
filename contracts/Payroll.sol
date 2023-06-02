b4e140a2a37129ad878106f5dc5592eedfd3e3b2
luckychacha <358140284@qq.com>
2023-06-02 17:32:38 +0800

    edit auto payroll and terminatpayroll.

--- contracts/Payroll.sol
+++ contracts/Payroll.sol
@@ -37,23 +37,40 @@ contract Payroll is
     ReentrancyGuard,
     Ownable
 {
-    event Logger(address a, uint256 oneYearPayment);
+    event Init(
+        uint256 timestamp,
+        uint256 oneYearPayment,
+        uint256 employersMappingLength
+    );
+    event TerminatedPayment(
+        uint256 timestamp,
+        address account,
+        uint256 unpaidSalary
+    );
+    event PayrollPayment(uint256 timestamp, address account, uint256 salary);
+    event TerminateEmployee(
+        uint256 timestamp,
+        address account,
+        uint256 terminationTime
+    );
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
     mapping(bytes4 => bool) private _supportedInterfaces;
 
     // 365/12
-    uint256 private constant PAYMENT_INTERVAL = 40 seconds;
-    // uint256 private constant PAYMENT_INTERVAL = 30.42 days;
-
+    uint256 private constant PAYMENT_INTERVAL = 30.42 days;
     uint256 private constant TERMINTATE_NOTICE_INTERVAL = 30 days;
+
+    // uint256 private constant PAYMENT_INTERVAL = 1 minutes;
+    // uint256 private constant TERMINTATE_NOTICE_INTERVAL = 55 seconds;
 
     uint256 public lastPaymentTimestamp;
     uint256 public employersMappingLength;
     IERC20 private _token;
     address[] public employeeAddressList;
     uint256[] public salaryAmountArray;
-    uint terminatedEmployeeCount;
+    uint public terminatedEmployeeCount;
+    uint256 public nextTerminatedTimestamp;
 
     struct Employee {
         address account;
@@ -101,32 +118,10 @@ contract Payroll is
         }
         uint256 oneYearPayment = calculateOneYearPayment(_salaryAmountArray);
         _mint(address(this), oneYearPayment);
-        approve(address(this), oneYearPayment);
         _token = IERC20(address(this));
         terminatedEmployeeCount = 0;
         lastPaymentTimestamp = block.timestamp;
-    }
-
-    // terminate
-    function terminate(
-        address _terminatedAddress
-    ) public MustBeOwnerOfContract {
-        for (uint i = 0; i < employersMappingLength; i++) {
-            if (employees[i].account == _terminatedAddress) {
-                employees[i].isTerminated = true;
-
-                uint256 terminationTime = block.timestamp +
-                    TERMINTATE_NOTICE_INTERVAL;
-                employees[i].terminationTime = terminationTime;
-                if (terminationTime > block.timestamp + PAYMENT_INTERVAL) {
-                    employees[i].hasFinalRoundPayment = false;
-                } else {
-                    employees[i].hasFinalRoundPayment = true;
-                }
-
-                terminatedEmployeeCount += 1;
-            }
-        }
+        emit Init(block.timestamp, oneYearPayment, employersMappingLength);
     }
 
     function safeTransfer(
@@ -151,6 +146,108 @@ contract Payroll is
         _supportedInterfaces[interfaceId] = true;
     }
 
+    // auto dispense
+    function performUpkeep(
+        bytes calldata performData
+    ) external override nonReentrant {
+        (bool upKeepNeeded, ) = checkUpkeep("");
+        if (!upKeepNeeded) {
+            revert SalaryPaymentUpkeepCannotBeFulfilled();
+        }
+
+        statusSalaryPayment = salaryPaymentStatus.SENDING;
+        bool performForTerminated = abi.decode(performData, (bool));
+        if (performForTerminated) {
+            if (terminatedEmployeeCount <= 0 || nextTerminatedTimestamp == 0) {
+                return;
+            }
+            terminatedEmployeePayment();
+        } else {
+            payrollPayment();
+        }
+        statusSalaryPayment = salaryPaymentStatus.OPEN;
+    }
+
+    function terminatedEmployeePayment() private {
+        uint256 minTimestamp = 0;
+        uint256 timestamp = block.timestamp;
+
+        for (uint i = 0; i < employersMappingLength; i++) {
+            if (
+                employees[i].account == address(0) || !employees[i].isTerminated
+            ) {
+                continue;
+            }
+            if (employees[i].terminationTime > timestamp) {
+                if (minTimestamp > employees[i].terminationTime) {
+                    minTimestamp = employees[i].terminationTime;
+                }
+                continue;
+            }
+            uint256 unpaidSeconds = employees[i].terminationTime -
+                lastPaymentTimestamp;
+            uint256 unpaidSalary = 0;
+            if (unpaidSeconds > 0) {
+                unpaidSalary = employees[i].salary.mul(unpaidSeconds).div(
+                    PAYMENT_INTERVAL
+                );
+                address addressToGetPaid = employees[i].account;
+                bool success = _token.transfer(addressToGetPaid, unpaidSalary);
+
+                if (!success) {
+                    revert CannotSendMoney();
+                }
+            }
+            emit TerminatedPayment(
+                block.timestamp,
+                employees[i].account,
+                unpaidSalary
+            );
+
+            employees[i].terminationTime = 0;
+            terminatedEmployeeCount -= 1;
+            delete employees[i];
+            delete employeeAddressList[i];
+            delete salaryAmountArray[i];
+        }
+
+        nextTerminatedTimestamp = minTimestamp;
+    }
+
+    function payrollPayment() private {
+        for (uint i = 0; i < employersMappingLength; i++) {
+            if (employees[i].account == address(0)) {
+                continue;
+            }
+            if (
+                employees[i].isTerminated && employees[i].hasFinalRoundPayment
+            ) {
+                continue;
+            }
+
+            address addressToGetPaid = employees[i].account;
+            bool success = _token.transfer(
+                addressToGetPaid,
+                employees[i].salary
+            );
+            if (!success) {
+                revert CannotSendMoney();
+            }
+            if (employees[i].isTerminated) {
+                employees[i].hasFinalRoundPayment = true;
+            }
+            emit PayrollPayment(
+                block.timestamp,
+                employees[i].account,
+                employees[i].salary
+            );
+        }
+        lastPaymentTimestamp = block.timestamp;
+    }
+
+    receive() external payable {}
+
+    // ======= View =======
     // implement IERC165
     // Implementers can declare support of contract interfaces.
     function supportsInterface(
@@ -180,7 +277,8 @@ contract Payroll is
         bool hasEmployee = (employersMappingLength > 0);
         bool statusIsOpen = (statusSalaryPayment == salaryPaymentStatus.OPEN);
 
-        bool performForTerminated = terminatedEmployeeCount > 0;
+        bool performForTerminated = (terminatedEmployeeCount > 0 &&
+            block.timestamp >= nextTerminatedTimestamp);
         bool performForPayroll = (
             (timestampDurationPass && hasEmployee && statusIsOpen)
         );
@@ -190,73 +288,6 @@ contract Payroll is
 
         performData = abi.encode(performForTerminated);
         return (upkeepNeeded, performData);
-    }
-
-    function performUpkeep(
-        bytes calldata performData
-    ) external override nonReentrant {
-        (bool upKeepNeeded, ) = checkUpkeep("");
-        if (!upKeepNeeded) {
-            revert SalaryPaymentUpkeepCannotBeFulfilled();
-        }
-
-        statusSalaryPayment = salaryPaymentStatus.SENDING;
-        bool performForTerminated = abi.decode(performData, (bool));
-
-        for (uint i = 0; i < employersMappingLength; i++) {
-            // performForTerminated
-            if (performForTerminated && terminatedEmployeeCount > 0) {
-                if (!employees[i].isTerminated) {
-                    continue;
-                }
-                uint256 unpaidSeconds = employees[i].terminationTime -
-                    lastPaymentTimestamp;
-
-                if (unpaidSeconds > 0) {
-                    uint unpaidSalary = employees[i]
-                        .salary
-                        .mul(unpaidSeconds)
-                        .div(PAYMENT_INTERVAL);
-                    address addressToGetPaid = employees[i].account;
-                    bool success = _token.transfer(
-                        addressToGetPaid,
-                        unpaidSalary
-                    );
-
-                    if (!success) {
-                        revert CannotSendMoney();
-                    }
-                }
-                employees[i].terminationTime = 0;
-                employees[i].isTerminated = true;
-                terminatedEmployeeCount -= 1;
-                delete employees[i];
-                delete employeeAddressList[i];
-                delete salaryAmountArray[i];
-            } else {
-                // performForPayroll
-                if (
-                    employees[i].isTerminated &&
-                    employees[i].hasFinalRoundPayment
-                ) {
-                    continue;
-                }
-
-                address addressToGetPaid = employees[i].account;
-                bool success = _token.transfer(
-                    addressToGetPaid,
-                    employees[i].salary
-                );
-                if (!success) {
-                    revert CannotSendMoney();
-                }
-                if (employees[i].isTerminated) {
-                    employees[i].hasFinalRoundPayment = true;
-                }
-                lastPaymentTimestamp = block.timestamp;
-            }
-        }
-        statusSalaryPayment = salaryPaymentStatus.OPEN;
     }
 
     function employeeAddressesMustBeUnique(
@@ -269,8 +300,6 @@ contract Payroll is
             }
         }
     }
-
-    receive() external payable {}
 
     function calculateOneYearPayment(
         uint256[] memory _salaryAmountArray
@@ -303,9 +332,46 @@ contract Payroll is
         return totalAmountRequiredEachPayment;
     }
 
+    // ============ Ownable =============
+    function terminate(
+        address _terminatedAddress
+    ) public MustBeOwnerOfContract {
+        for (uint i = 0; i < employersMappingLength; i++) {
+            if (employees[i].account == _terminatedAddress) {
+                employees[i].isTerminated = true;
+
+                uint256 terminationTime = block.timestamp +
+                    TERMINTATE_NOTICE_INTERVAL;
+                employees[i].terminationTime = terminationTime;
+                if (terminationTime > lastPaymentTimestamp + PAYMENT_INTERVAL) {
+                    employees[i].hasFinalRoundPayment = false;
+                } else {
+                    employees[i].hasFinalRoundPayment = true;
+                }
+
+                terminatedEmployeeCount += 1;
+                if (nextTerminatedTimestamp == 0) {
+                    nextTerminatedTimestamp = terminationTime;
+                }
+                emit TerminateEmployee(
+                    block.timestamp,
+                    employees[i].account,
+                    terminationTime
+                );
+
+                break;
+            }
+        }
+    }
+
+    function mintOneYearPayment() public MustBeOwnerOfContract {
+        uint256 oneYearPayment = calculateOneYearPayment(salaryAmountArray);
+        _mint(address(this), oneYearPayment);
+    }
+
     //  ======= modifiers =======
     modifier MustBeOwnerOfContract() {
-        if (msg.sender == owner()) {
+        if (msg.sender != owner()) {
             revert NotContractOwner();
         }
         _;
